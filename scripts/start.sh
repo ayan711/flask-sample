78810341f30399eaf5df59d141068677b0b17d95
ayan711 <31046045+ayan711@users.noreply.github.com>
2024-09-12 12:43:39 +0530

    added appspec

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
+docker pull ayan711/flask-app:latest
 
 # Run the Docker image as a container
-echo
+docker -d -p 5000:5000 ayan711/flask-app:latest
